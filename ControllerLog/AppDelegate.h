689ee585185b337be9f57861ca0c4922320e972c
Ali <604999439@qq.com>
2016-08-25 09:50:56 +0800

    Initial Commit

---
+++ ControllerLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControllerLog
+//
+//  Created by Almas on 16/8/25.
+//  Copyright © 2016年 Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ControllerLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControllerLog
+//
+//  Created by Almas on 16/8/25.
+//  Copyright © 2016年 Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
